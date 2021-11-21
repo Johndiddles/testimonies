42e309ae19982e815c35156e2764623b2a477a89
Johndiddles <johnadepelumi@gmail.com>
2021-11-21 08:04:11 +0000

    changed buttons to images

--- script.js
+++ script.js
@@ -12,32 +12,32 @@ const data = [
     {
         id: 1,
         name: 'John',
-        imageUrl: 'somewhere',
+        imageUrl: './images/gee.png',
         testimony: 'I have a testimony to share'
     },
     {
         id: 2,
         name: 'Morenike',
-        imageUrl: 'somewhere',
+        imageUrl: './images/she.png',
         testimony: 'John finally opened door for me o'
     },
     {
         id: 3,
         name: 'Mark',
-        imageUrl: 'somewhere',
+        imageUrl: './images/nuel.png',
         testimony: 'Glory, interns did not murder me durning HNGi8'
     },
     {
         id: 4,
         name: 'John Doe',
-        imageUrl: 'somewhere',
+        imageUrl: './images/gee.png',
         testimony: 'Someone remembers me for no reason'
     },
 
     {
         id: 5,
         name: 'SeunBayo',
-        imageUrl: 'somewhere',
+        imageUrl: './images/nuel.png',
         testimony: 'I survived all the spiritual attacks'
     }
 ]
@@ -57,10 +57,9 @@ for (let i=0; i<data.length; i++){
         const testimonies = data[i];
 
     document.getElementById('left').innerHTML += `
-    <button 
+    <img src=${JSON.stringify(testimonies.imageUrl)}
         id=${data[i].id}
         onclick='handleBtn(${JSON.stringify(testimonies.testimony)}, ${JSON.stringify(testimonies.name)})'
-    >
-        ${data[i].name}
-    </button>`
+        alt=${data[i].name}
+    >`
 }
